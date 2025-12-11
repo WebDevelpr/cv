2a2973b5fa5ce0d84e6305fad461d3a5abbbd762
Dmytro <gudzoniya111@gmail.com>
2025-12-11 22:06:33 +0200

    add education section to home-page

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -2,10 +2,10 @@ export const HomePage = () => {
   return (
     <>
       <section className='container mt-12'>
-        <div className='flex h-12 w-42 items-center justify-center bg-black'>
-          <h2 className='text-xl font-semibold text-white'>PERSONAL DATA</h2>
+        <div className='flex h-12 w-max items-center justify-center bg-black'>
+          <h2 className='m-1 text-xl font-semibold text-white'>PERSONAL DATA</h2>
         </div>
-        <div className='mt-4'>
+        <div className='mt-4 space-y-2 text-lg'>
           <p>49A Dehtiarivska Street, 01001, Kyiv, Kyiv City, Ukraine </p>
           <p>gudzoniya111@gmail.com</p>
           <p>Date birth 26.08.2007</p>
@@ -21,7 +21,30 @@ export const HomePage = () => {
           </p>
         </div>
       </section>
-      <section></section>
+      <section className='container mt-12'>
+        <div className='flex h-12 w-max items-center justify-center bg-black'>
+          <h2 className='m-1 text-xl font-semibold text-white'>EDUCATION</h2>
+        </div>
+        <div className='mt-4 space-y-2 text-lg'>
+          <div className='flex justify-between'>
+            <p className='font-semibold'>
+              Kyiv National Economic University named after Vadym Hetman
+            </p>
+            <p className='text-gray-400'>2024 - Present</p>
+          </div>
+          <p>
+            Bachelor’s Degree, Computer Science (Specialty 122){' '}
+            <span className='font-semibold'>3nd-year student</span>
+          </p>
+          <div className='mt-8'>
+            <p>
+              Currently studying core areas of computer science, including
+              algorithms, data structures, software development principles, and
+              modern web technologies.
+            </p>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
