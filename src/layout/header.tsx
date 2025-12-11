5bb75b726b50de2d5e76933dbbbe79c4b393bb05
Dmytro <gudzoniya111@gmail.com>
2025-12-11 16:39:46 +0200

    add header

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,3 +1,11 @@
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="container mt-4">
+      <div className=" justify-between flex items-center">
+        <h1 className="font-bold  text-6xl">DMYTRO LEBEICHUK</h1>
+      <img src="src/public/images/avatar.jpg" alt="Avatar " className="w-48  h-48" loading="lazy" />
+      </div>
+      <div className="w-full h-3 mt-6 bg-black"></div>
+    </header>
+  )
 };
