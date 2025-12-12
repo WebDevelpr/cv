aad9031a799e955f94add38de55917e1b727a2ef
Dmytro <gudzoniya111@gmail.com>
2025-12-12 22:55:29 +0200

    change language and skills section as ShadCn components

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,24 +1,6 @@
+import { Progress } from '@/components/ui/progress'
+
 export const HomePage = () => {
-  const skills = [
-    { name: 'HTML', level: 'w-[95%]' },
-    { name: 'CSS', level: 'w-[85%]' },
-    { name: 'JavaScript', level: 'w-[70%]' },
-    { name: 'TypeScript', level: 'w-[75%]' },
-    { name: 'Tailwind', level: 'w-[85%]' },
-    { name: 'React', level: 'w-[75%]' },
-    { name: 'Vite', level: 'w-[80%]' },
-    { name: 'Git', level: 'w-[85%]' },
-    { name: 'GitHub', level: 'w-[70%]' },
-    { name: 'ShadCn', level: 'w-[90%]' }
-  ]
-
-  const language = [
-    { name: 'Ukrainian', level: 'w-[100%]' },
-    { name: 'English', level: 'w-[85%]' },
-    { name: 'Poland', level: 'w-[40%]' },
-    { name: 'Russian', level: 'w-[90%]' }
-  ]
-
   return (
     <>
       <section className='container mt-12'>
@@ -35,6 +17,7 @@ export const HomePage = () => {
               href='https://linkedin.com/in/dmytro-lebeichuk-1417513a0'
               target='_blank'
               rel='noopener noreferrer'
+              className='text-blue-500'
             >
               LinkedIn
             </a>
@@ -102,16 +85,48 @@ export const HomePage = () => {
         <div className='flex h-12 w-max items-center justify-center bg-black'>
           <h2 className='m-1 text-xl font-semibold text-white'>SKILLS</h2>
         </div>
-        <div className='mt-4 grid grid-cols-2 gap-4'>
-          {skills.map((skill) => (
-            <div key={skill.name}>
-              <p className='font-medium'>{skill.name}</p>
+        <div className='mt-4 grid grid-cols-2 gap-6'>
+          <div>
+            <h2 className='mb-1 font-semibold'>HTML</h2>
+            <Progress value={95} />
+          </div>
+          <div>
+            <h2 className='mb-1 font-semibold'>CSS</h2>
+            <Progress value={90} />
+          </div>
+          <div>
+            <h2 className='mb-1 font-semibold'>Tailwind</h2>
+            <Progress value={95} />
+          </div>
+          <div>
+            <h2 className='mb-1 font-semibold'>JavaScript</h2>
+            <Progress value={75} />
+          </div>
+          <div>
+            <h2 className='mb-1 font-semibold'>TypeScript</h2>
+            <Progress value={70} />
+          </div>
 
-              <div className='mt-1 h-2 w-full bg-gray-300'>
-                <div className={`h-full bg-black ${skill.level}`}></div>
-              </div>
-            </div>
-          ))}
+          <div>
+            <h2 className='mb-1 font-semibold'>React</h2>
+            <Progress value={80} />
+          </div>
+          <div>
+            <h2 className='mb-1 font-semibold'>Git</h2>
+            <Progress value={85} />
+          </div>
+          <div>
+            <h2 className='mb-1 font-semibold'>Vite</h2>
+            <Progress value={90} />
+          </div>
+          <div>
+            <h2 className='mb-1 font-semibold'>GitHub</h2>
+            <Progress value={90} />
+          </div>
+          <div>
+            <h2 className='mb-1 font-semibold'>ShadCn</h2>
+            <Progress value={90} />
+          </div>
         </div>
       </section>
       <section className='container mt-12'>
@@ -119,15 +134,22 @@ export const HomePage = () => {
           <h2 className='m-1 text-xl font-semibold text-white'>LANGUAGE</h2>
         </div>
         <div className='mt-4 grid grid-cols-2 gap-4'>
-          {language.map((language) => (
-            <div key={language.name}>
-              <p className='font-medium'>{language.name}</p>
-
-              <div className='mt-1 h-2 w-full bg-gray-300'>
-                <div className={`h-full bg-black ${language.level}`}></div>
-              </div>
-            </div>
-          ))}
+          <div>
+            <h2 className='mb-1 font-semibold'>Ukrainian</h2>
+            <Progress value={100} />
+          </div>
+          <div>
+            <h2 className='mb-1 font-semibold'>English</h2>
+            <Progress value={85} />
+          </div>
+          <div>
+            <h2 className='mb-1 font-semibold'>Russian</h2>
+            <Progress value={95} />
+          </div>
+          <div>
+            <h2 className='mb-1 font-semibold'>Polish</h2>
+            <Progress value={40} />
+          </div>
         </div>
       </section>
     </>
