98ce3d5d2bdb59b672dcd0e74885edc893f627b7
Dmytro <gudzoniya111@gmail.com>
2025-12-13 20:22:56 +0200

    some changes with assets

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,12 +1,14 @@
+import avatar from '@/assets/avatar.jpg'
+
 export const Header = () => {
   return (
     <header className='container mt-4'>
       <div className='flex items-center justify-between'>
         <h1 className='text-6xl font-bold'>DMYTRO LEBEICHUK</h1>
         <img
-          src='src/public/images/avatar.jpg'
-          alt='Avatar '
-          className='h-48 w-48'
+          src={avatar}
+          alt='Avatar'
+          className='h-48 w-48 rounded-full'
           loading='lazy'
         />
       </div>
