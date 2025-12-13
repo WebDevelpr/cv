454c05eaf77cd656b25f77b4a383017604de0d6d
Dmytro <gudzoniya111@gmail.com>
2025-12-14 00:51:37 +0200

    adapted header layout for responsiveness using media queries

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -3,15 +3,19 @@ import avatar from '@/assets/avatar.jpg'
 export const Header = () => {
   return (
     <header className='container mt-4'>
-      <div className='flex items-center justify-between'>
-        <h1 className='text-6xl font-bold'>DMYTRO LEBEICHUK</h1>
+      <div className='flex flex-col items-center justify-between gap-4 sm:flex-row'>
+        <h1 className='text-center text-2xl font-bold sm:text-left sm:text-4xl lg:text-6xl'>
+          DMYTRO LEBEICHUK
+        </h1>
+
         <img
           src={avatar}
           alt='Avatar'
-          className='s h-48 w-48'
+          className='h-32 w-32 sm:h-48 sm:w-48'
           loading='lazy'
         />
       </div>
+
       <div className='mt-6 h-3 w-full bg-black'></div>
     </header>
   )
